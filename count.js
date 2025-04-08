bce41f35b381905b62ce7b3622ad4240ba038dfe
hjlee <hjlee@digicaps.com>
2025-04-08 17:57:21 +0900

    increase 함수가 증가값을 인수로 받아 사용하도록 수정 (close #4)

--- count.js
+++ count.js
@@ -1,5 +1,5 @@
 export let count = 0;
 
-export function increment() {
-  return (count += 1);
+export function increase(n = 1) {
+  return (count += n);
 }
--- test/count.test.js
+++ test/count.test.js
@@ -1,12 +1,12 @@
 import assert from 'assert';
-import { count, increment } from '../count.js';
+import { count, increase } from '../count.js';
 
-it('increment 함수를 호출하면, count 데이터가 1 증가!', () => {
+it('increase 함수를 호출하면, count 데이터가 1 증가!', () => {
   assert.equal(count, 0);
-  increment();
+  increase();
   assert.equal(count, 1); // 성공!
 });
 
-it('increment 함수를 호출하면, count 값을 반환!', () => {
-  assert.equal(increment(), count);
+it('increase 함수를 호출하면, count 값을 반환!', () => {
+  assert.equal(increase(), count);
 });
